21d5b09cff07882fabcf037c89fa30e62b7691b8
shashikumar321 <shashikumaryn3@gmail.com>
2022-03-04 10:33:18 +0530

    Initial commit for ui app

---
+++ src/components/AppContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header.js";
+
+const AppContainer = () => {
+
+    return <div>
+        <Header />
+    </div>
+
+};
+
+export default AppContainer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+
+    return <div class="header-panel">
+        <h1>NightOWl</h1>
+    </div>
+
+};
+
+export default Header;
